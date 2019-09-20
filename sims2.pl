5aa8b07ead7df97324d2e7bd1390ae1e4c51df37
William9923 <williamong1400@gmail.com>
2019-09-20 21:50:49 +0700

    Added all condition

--- sims2.pl
+++ sims2.pl
@@ -44,14 +44,81 @@ cek() :-
 	write('Fun     = '),writeln(Y),
 	write('Energy  = '),writeln(Z).
 
+validasi(X) :- 
+	(X == "Tidur Siang", call(tidur("Siang")));
+	(X == "Tidur Malam", call(tidur("Malam")));
+	(X == "Makan Hamburger", call(makan("Hamburger")));
+	(X == "Makan Pizza", call(makan("Pizza")));
+	(X == "Makan Steak and Beans", call(makan("Steak and Beans")));
+	(X == "Minum Air", call(minum("Air")));
+	(X == "Minum Kopi", call(minum("Kopi")));
+	(X == "Minum Jus", call(minum("Jus")));
+	(X == "Buang Air Kecil", call(buang_air("Kecil")));
+	(X == "Buang Air Besar", call(buang_air("Besar")));
+	(X == "Bersosialisasi ke Kafe", call(bersosialisasi_ke_kafe()));
+	(X == "Bermain Media Sosial", call(bermain_media_sosial()));
+	(X == "Bermain komputer", call(bermain_komputer()));
+	(X == "Mandi", call(mandi()));
+	(X == "Cuci Tangan", call(cuci_tangan()));
+	(X == "Mendengarkan Musik di Radio", call(mendengarkan_musik_di_radio()));
+	(X == "Membaca Koran", call(membaca("Koran")));
+	(X == "Membaca Novel", call(membaca("Novel")));
+	(call(tes())).
+
 % Aksi dan konsekuensinya
+tes() :-
+	writeln("Command tidak ditemukan").
+
+tidur(X) :-
+	(addStatus(0,0,10), X == "Siang");
+	(addStatus(0,0,15), X == "Malam").
+
 mandi() :-
-	addStatus(10,0,0).
+	addStatus(15,0,-5).
+
+bersosialisasi_ke_kafe() :- 
+	addStatus(-5,15,-10).
+
+bermain_media_sosial() :-
+	addStatus(0,10,-10).
+
+bermain_komputer() :-
+	addStatus(0,15,-10).
+
+cuci_tangan() :-
+	addStatus(5,0,0).
+
+mendengarkan_musik_di_radio() :-
+	addStatus(0,10,-5).
+
+membaca(X) :- 
+	("Koran" == X, addStatus(0,5,-5));
+	("Novel" == X, addStatus(0,10,-5)).
+
+makan(X) :-
+	(X == "Hamburger",addStatus(0,0,5));
+	(X == "Pizza",addStatus(0,0,10));
+	(X == "Steak and Beans",addStatus(0,0,15)).
+
+minum(X) :-
+	(X == "Air",addStatus(-5,0,0));
+	(X == "Kopi",addStatus(-10,0,5));
+	(X == "Jus",addStatus(-5,0,10)).
+
+buang_air(X) :-
+	(X == "Kecil",addStatus(5,0,0));
+	(X == "Besar",addStatus(10,0,-5)).
 
 % Main Loop
 game :-
     repeat,
     write('> '),
     read(X),
-    call(X),
+    writeln(X),
+    validasi(X),
     game.
+
+% ketika masukan tidak valid (komenan salah)
+%% game :-
+%% 	writeln("Command tidak valid."),
+%% 	game.
